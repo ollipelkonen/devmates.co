2c0f3763647441e64befd2d03e15365ed38042db
olli pelkonen <olli.pelkonen@kapsi.fi>
2019-10-04 16:43:53 +0300

    04.10.2019 grouping anagrams -problem. python

---
+++ 2019-10-04/anagrams.py
@@ -0,0 +1,34 @@
+
+#  Problem of the day
+#  
+#  Given an array of strings, group anagrams together.
+#  
+#  Example:
+#  
+#                        
+#    Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+#    Output: 
+#      [
+#        ["ate","eat","tea"],
+#        ["nat","tan"],
+#        ["bat"]
+#      ]
+
+
+
+
+Input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+
+dic = dict()
+for a in Input:
+  key = ''.join(sorted(a)) 
+  if not key in dic:
+    dic[key] = [a]
+  else:
+    dic[key].append(a)
+
+output = dic.values()
+
+print(output)
+
